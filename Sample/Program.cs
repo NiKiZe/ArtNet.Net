c793c1ff11dabb0c31bcb4914d25259ea68e26a4
Bruno Angeles <bruno.angeles@gmail.com>
2016-01-20 17:16:57 -0500

    Log packet length and universe. Use a static array for the previous values received, and put the listener in its own function. Also use MF values for IP address.

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -7,42 +7,45 @@ namespace Sample
 {
     class MainClass
     {
+        public static byte[] dmxData = new byte[511];
+
         public static void Main(string[] args)
         {
             var artnet = new ArtNet.Sockets.ArtNetSocket();
             artnet.EnableBroadcast = true;
 
             Console.WriteLine(artnet.BroadcastAddress.ToString());
-            artnet.Open(IPAddress.Parse("127.0.0.1"), IPAddress.Parse("255.255.255.0"));
+            artnet.Open(IPAddress.Parse("10.10.80.48"), IPAddress.Parse("255.255.255.0"));
 
             byte[] _dmxData = new byte[511];
 
-            artnet.NewPacket += (object sender, ArtNet.Sockets.NewPacketEventArgs<ArtNet.Packets.ArtNetPacket> e) => 
-            {
-                if(e.Packet.OpCode == ArtNet.Enums.ArtNetOpCodes.Dmx)
-                {  
-                    var packet = e.Packet as ArtNet.Packets.ArtNetDmxPacket;
-                        Console.Clear();
-
-                        if(packet.DmxData != _dmxData)
-                        {
-                            Console.WriteLine("New Packet");
-                            for(var i = 0; i < packet.DmxData.Length; i++)
-                            {
-                                if(packet.DmxData[i] != 0)
-                                    Console.WriteLine(i + " = " + packet.DmxData[i]);
-
-                            };
-
-                            _dmxData = packet.DmxData;
-                        }
-                    }
-            };
-            
+            // Register a packet listener
+            artnet.NewPacket += artnet_NewPacket;
             Console.ReadLine();
-
         }
 
-        byte[] _dmxData = new byte[511];
+        static void artnet_NewPacket(object sender, NewPacketEventArgs<ArtNetPacket> e)
+        {
+            if (e.Packet.OpCode == ArtNet.Enums.ArtNetOpCodes.Dmx)
+            {
+                var packet = e.Packet as ArtNet.Packets.ArtNetDmxPacket;
+                Console.Clear();
+
+                if (packet.DmxData != dmxData)
+                {
+                    // Data has changed
+                    Console.WriteLine("New Packet of length " + packet.DmxData.Length.ToString() + " in universe " + packet.Universe.ToString());
+                    for (var i = 0; i < packet.DmxData.Length; i++)
+                    {
+                        if (packet.DmxData[i] != 0)
+                        {
+                            Console.WriteLine(i + " = " + packet.DmxData[i]);
+                        }
+
+                    };
+                    dmxData = packet.DmxData;
+                }
+            }
+        }
     }
 }
