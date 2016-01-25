12f762b6627e58e3359ccaabde40ab2792ff017a
Bruno Angeles <bruno.angeles@gmail.com>
2016-01-25 16:17:57 -0500

    Store Art-Net values as Color objects before dumping to images. Art-Net sender sends to all specified universes.

--- Sample/ProgramReceiver.cs
+++ Sample/ProgramReceiver.cs
@@ -6,14 +6,20 @@ using ArtNet.Packets;
 using System.IO;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
+using System.Timers;
+using System.Threading;
 
 namespace Sample
 {
     class MainClass
     {
-        public static byte[] dmxData = new byte[511];
         public const uint MAX_SUPPORTED_UNIVERSES = 512;
+        public static byte[,] s_dmxValues = new byte[MAX_SUPPORTED_UNIVERSES, MAX_SUPPORTED_UNIVERSES]; // rows = universes, columns = values
+        public static Color[,] s_dmxColours = new Color[MAX_SUPPORTED_UNIVERSES, (int)Math.Ceiling((double)MAX_SUPPORTED_UNIVERSES / 3)]; // rows = universes, columns = colours (512 channels gives 171 colours)
+        public const uint IMAGE_SEQUENCE_FPS = 60; // iterations per second
         public static int s_currentIndex = 0;
+        public static bool s_writeImageSequence = true;
+        public static Mutex s_mutex = new Mutex(); // a mutex to prevent concurrent read/write of the DMX data
 
         public static void Main(string[] args)
         {
@@ -23,9 +29,55 @@ namespace Sample
             Console.WriteLine(artnet.BroadcastAddress.ToString());
             artnet.Open(IPAddress.Parse("10.10.80.48"), IPAddress.Parse("255.255.255.0"));
 
-            // Register a packet listener
+            // Initialize DMX values and colours to zero
+            for (int i = 0; i < MAX_SUPPORTED_UNIVERSES; i++)
+            {
+                for (int j = 0; j < MAX_SUPPORTED_UNIVERSES; j++)
+                {
+                    s_dmxValues[i, j] = 0;
+                }
+            }
+
+            // Register a packet listener that will write the data to the array at the incoming frame rate
             artnet.NewPacket += artnet_NewPacket;
+
+            if (s_writeImageSequence)
+            {
+                // Launch an image writer
+                System.Timers.Timer timer = new System.Timers.Timer(1000 / IMAGE_SEQUENCE_FPS);
+                timer.Elapsed += WriteImage;
+                timer.Start();
+            }
+
             Console.ReadLine();
+        }
+
+        private static void WriteImage(Object source, ElapsedEventArgs e)
+        {
+            s_mutex.WaitOne();
+            // Generate an image with the current state of the DMX values
+            DirectoryInfo di = new DirectoryInfo(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\sequence");
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+            string fileNameNoExt = "ImageSequence_" + s_currentIndex.ToString("D8");
+            FileInfo fi = new FileInfo(di.FullName + "\\" + fileNameNoExt + ".png");
+            int imgWidth = (int)Math.Ceiling((double)MAX_SUPPORTED_UNIVERSES / 3);
+            int imgHeight = (int)MAX_SUPPORTED_UNIVERSES;
+
+            var im = new Bitmap(imgWidth, imgHeight, PixelFormat.Format24bppRgb);
+            for (int row = 0; row < s_dmxColours.GetLength(0); row++)
+            {
+                for (int col = 0; col < s_dmxColours.GetLength(1); col++)
+                {
+                    im.SetPixel(col, row, s_dmxColours[row, col]);
+                }
+            }
+            im.Save(fi.FullName, ImageFormat.Png);
+
+            s_currentIndex++;
+            s_mutex.ReleaseMutex();
         }
 
         static void artnet_NewPacket(object sender, NewPacketEventArgs<ArtNetPacket> e)
@@ -33,46 +85,20 @@ namespace Sample
             if (e.Packet.OpCode == ArtNet.Enums.ArtNetOpCodes.Dmx)
             {
                 var packet = e.Packet as ArtNet.Packets.ArtNetDmxPacket;
-                //Console.Clear();
+                Console.Clear();
 
-                if (packet.DmxData != dmxData)
+                s_mutex.WaitOne();
+                Console.WriteLine("New Packet of length " + packet.DmxData.Length.ToString() + " in universe " + packet.Universe.ToString());
+                // We expect RGB values (1 byte/channel)
+                for (var i = 0; i < packet.DmxData.Length / 3; i++)
                 {
-                    // Data has changed
-                    Console.WriteLine("New Packet of length " + packet.DmxData.Length.ToString() + " in universe " + packet.Universe.ToString());
-                    for (var i = 0; i < packet.DmxData.Length; i++)
-                    {
-                        if (packet.DmxData[i] != 0)
-                        {
-                            Console.WriteLine(DateTime.Now.ToString() + " " + i + " = " + packet.DmxData[i]);
-                        }
-                    };
-                    dmxData = packet.DmxData;
-
-                    // Initialize the byte array with zeros
-                    var byteArray = new byte[MAX_SUPPORTED_UNIVERSES * 512];
-                    for (int i = 0; i < byteArray.Length; i++)
-                    {
-                        byteArray[i] = 0;
-                    }
-                    // Populate with incoming dmx Data
-                    for (int i = 0; i < packet.DmxData.Length; i++)
-                    {
-                        byteArray[i] = packet.DmxData[i];
-                    }
-
-                    // Generate an image
-                    DirectoryInfo di = new DirectoryInfo(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\sequence");
-                    if (!di.Exists)
-                    {
-                        di.Create();
-                    }
-                    string fileNameNoExt = "ImageSequence_" + s_currentIndex.ToString("D8");
-                    FileInfo fi = new FileInfo(di.FullName + "\\" + fileNameNoExt + ".png");
-                    var im = new Bitmap(512, (int)MAX_SUPPORTED_UNIVERSES, 512, PixelFormat.Format8bppIndexed, Marshal.UnsafeAddrOfPinnedArrayElement(byteArray, 0));
-                    im.Save(fi.FullName, ImageFormat.Png);
-
-                    s_currentIndex++;
-                }
+                    s_dmxValues[packet.Universe, i*3] = packet.DmxData[i];
+                    s_dmxValues[packet.Universe, i*3 + 1] = packet.DmxData[i*3 + 1];
+                    s_dmxValues[packet.Universe, i*3 + 2] = packet.DmxData[i*3 + 2];
+                    Color current = Color.FromArgb(packet.DmxData[i*3], packet.DmxData[i*3 + 1], packet.DmxData[i*3 + 2]);
+                    s_dmxColours[packet.Universe, i] = current;
+                };
+                s_mutex.ReleaseMutex();
             }
         }
     }
--- Sample/ProgramSender.cs
+++ Sample/ProgramSender.cs
@@ -9,17 +9,18 @@ namespace Sample
 {
     class MainClass
     {
-        public const uint SENDER_FPS = 40; // iterations per second
+        public const uint SENDER_FPS = 44; // iterations per second
+        public const uint SENDER_UNIVERSES = 512; // number of universes to send
         public const uint SENDER_LENGTH = 512; // number of bytes to send
-        public static byte[] dmxData = new byte[511];
-        public static ArtNetSocket artnet = new ArtNetSocket();
+        public static byte[] s_dmxData = new byte[511];
+        public static ArtNetSocket s_artnet = new ArtNetSocket();
 
         public static void Main(string[] args)
         {
-            artnet.EnableBroadcast = true;
+            s_artnet.EnableBroadcast = true;
 
-            Console.WriteLine("Broadcast address: " + artnet.BroadcastAddress.ToString() + "\nAt FPS=" + SENDER_FPS + "\n");
-            artnet.Open(IPAddress.Parse("10.10.80.48"), IPAddress.Parse("255.255.255.0"));
+            Console.WriteLine("Broadcast address: " + s_artnet.BroadcastAddress.ToString() + "\nAt FPS=" + SENDER_FPS + "\n");
+            s_artnet.Open(IPAddress.Parse("10.10.80.48"), IPAddress.Parse("255.255.255.0"));
 
             Timer timer = new Timer(1000/SENDER_FPS);
             timer.Elapsed += SendArtNet;
@@ -27,19 +28,32 @@ namespace Sample
             Console.ReadLine();
         }
 
+        // We send RGB values (1 byte/channel) via Art-Net.
         private static void SendArtNet(Object source, ElapsedEventArgs e)
         {
             ArtNetDmxPacket toSend = new ArtNetDmxPacket();
             Random rnd = new Random();
-            toSend.Universe = 0; //(short)rnd.Next(0, 500);
 
             toSend.DmxData = new byte[SENDER_LENGTH];
-            for (int i = 0; i < SENDER_LENGTH; i++ )
+            for (uint universe = 0; universe < SENDER_UNIVERSES; universe++)
             {
-                toSend.DmxData[i] = (byte)rnd.Next(0, 255);
+                toSend.Universe = (short)universe;
+                for (uint i = 0; i < SENDER_LENGTH; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        var phase = 2 * Math.PI * i / SENDER_LENGTH; // radians
+                        uint redValue = (uint)(Math.Cos(DateTime.Now.Millisecond + phase) * 255.0); // add phase based on i index
+                        toSend.DmxData[i] = (byte)redValue; //(byte)rnd.Next(0, 255);
+                    }
+                    else
+                    {
+                        toSend.DmxData[i] = 0;
+                    }
+                }
+                s_artnet.Send(toSend);
+                Console.WriteLine("Sending [" + toSend.DmxData.Length + "] Art-Net values data on universe [" + toSend.Universe + "]");
             }
-            artnet.Send(toSend);
-            Console.WriteLine("Sending [" + toSend.DmxData.Length + "] values Art-Net data on universe [" + toSend.Universe + "]");
         }
     }
 }
